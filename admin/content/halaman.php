12f79e25ae7cb1851f16225206fc93b838505549
ResalR92 <resalramdahadi92@gmail.com>
2016-11-22 16:23:55 +0700

    Menampilkan Data Halaman

--- admin/content/halaman.php
+++ admin/content/halaman.php
@@ -10,6 +10,29 @@
 	switch($show){
 		default:
 			//Skrip menampilkan data
+			echo '<h3 class="page-header"><b>Daftar Halaman</b>';
+
+			if($_SESSION['leveluser'] == "admin"){
+				echo '<a href="'.$link.'&show=form" class="btn btn-primary btn-sm pull-right top-bottom">
+							<i class="glyphicon glyphicon-plus-sign"></i> Tambah
+						</a>';
+			}
+			echo '</h3>';
+
+			buka_tabel(array("Judul Halaman","User"));
+				$no = 1;
+				$query = $mysqli->query("SELECT * FROM halaman ORDER BY id_halaman");
+				while($data = $query->fetch_array()){
+					$user = $mysqli->query("SELECT nama_lengkap FROM user WHERE id_user='$data[id_user]'");
+					$us = $user->fetch_array();
+
+					if($_SESSION['leveluser'] == 'admin'){
+						isi_tabel($no, array($data['judul'], $us['nama_lengkap']), $link, $data['id_halaman']);
+					}else{
+						isi_tabel($no, array($data['judul'], $us['nama_lengkap']), $link, $data['id_halaman'], true, false);
+					}
+				}
+			tutup_tabel();
 		break;
 
 		case "form":
