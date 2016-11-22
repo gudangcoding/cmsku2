549496d95d8a3505bdf3bad7516d6e138dbe5d8b
ResalR92 <resalramdahadi92@gmail.com>
2016-11-22 16:10:59 +0700

    Struktur File Manajemen Halaman

---
+++ admin/content/halaman.php
@@ -0,0 +1,26 @@
+<script type="text/javascript" src="../plugin/tinymce/tinymce.min.js"></script>
+<script type="text/javascript" src="js/tinymce_config.js"></script>
+
+<?php
+	if(!defined("INDEX")) header('location: index.php');
+
+	$show = isset($_GET['show']) ? $_GET['show'] : "";
+	$link = "?content=halaman";
+
+	switch($show){
+		default:
+			//Skrip menampilkan data
+		break;
+
+		case "form":
+			//Skrip menampilkan form tambah dan edit data
+		break;
+
+		case "action":
+			//Skrip aksi form tambah dan edit
+		break;
+
+		case "delete":
+			//Skrip menghapus data
+		break;
+	}
