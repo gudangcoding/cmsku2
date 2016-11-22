a627e1b0fdbca59311a7f3eaa2683f7decf9cf0a
ResalR92 <resalramdahadi92@gmail.com>
2016-11-22 14:27:47 +0700

    Menampilkan Data Artikel

--- admin/content/artikel.php
+++ admin/content/artikel.php
@@ -10,6 +10,31 @@
 	switch($show){
 		default:
 			//Skrip menampilkan data
+			echo '<h3 class="page-header"><b>Daftar Artikel</b>
+						<a href="'.$link.'&show=form" class="btn btn-primary btn-sm pull-right top-button">
+							<i class="glyphicon glyphicon-plus-sign"></i> Tambah
+						</a>
+					</h3>';
+		
+			buka_tabel(array("Judul Artikel", "Kategori", "User", "Tanggal Posting"));
+			$no = 1;
+			$id_user = $_SESSION['iduser'];
+			
+			if($_SESSION['leveluser']=="admin") $query = $mysqli->query("SELECT * FROM artikel ORDER BY id_artikel DESC");
+			else $query = $mysqli->query("SELECT * FROM artikel WHERE id_user='$id_user' ORDER BY id_artikel");
+			while($data = $query->fetch_array()){
+				$user = $mysqli->query("SELECT nama_lengkap FROM user where id_user='$data[id_user]'");
+				$us = $user->fetch_array();
+				
+				$kategori = $mysqli->query("SELECT * FROM kategori where id_kategori='$data[kategori]'");
+				$kat = $kategori->fetch_array();
+				
+				$tanggal = tgl_indonesia($data['tanggal']);
+				
+				isi_tabel($no, array($data['judul'], $kat['kategori'], $us['nama_lengkap'], $tanggal), $link, $data['id_artikel']);
+				$no++;
+			} 
+			tutup_tabel();
 		break;
 
 		case "form":
