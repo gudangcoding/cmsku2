c6c8c5ebe950f58f8752b1b9eb0848f972e8f0ab
ResalR92 <resalramdahadi92@gmail.com>
2016-11-23 13:41:53 +0700

    Menampilkan Data Komentar

--- admin/content/komentar.php
+++ admin/content/komentar.php
@@ -11,6 +11,36 @@
 
 		default:
 			//Skrip Menampilkan data
+			echo '<h3 class="page-header"><b>Daftar Komentar</b></h3>';
+
+			buka_tabel(array("Author","Komentar","Artikel"));
+				$no = 1;
+				$query = $mysqli->query("SELECT * FROM komentar ORDER BY id_komentar desc");
+
+				while($data = $query->fetch_array()){
+					$artikel = $mysqli->query("SELECT * FROM artikel where id_artikel='$data[id_artikel]'");
+					$atk = $artikel->fetch_array();
+
+					$tanggal = tgl_indonesia($data['tanggal']);
+					if($data['dibaca'] == 'N'){
+						$author = '<a href="mailto:'.$data['email'].'">
+										<b>'.$data['nama'].'</b>
+									</a>';
+						$komentar = '<small class="text-muted">'.$tanggal.'</small>
+									<br><p><b>'.$data['komentar'].'</b></p>';
+					} else{
+						$author = '<a href="mailto:'.$data['email'].'">
+										'.$data['nama'].'
+									</a>';
+						$komentar = '<small class="text-muted">'.$tanggal.'</small>
+									<br><p>'.$data['komentar'].'</p>';
+					}
+					$artikel = '<a href="../artikel/'.$atk['id_artikel'].'/'.$atk['judul_seo'].'" target="blank">'.$atk['judul'].'</a>';
+
+					isi_tabel($no, array($author, $komentar, $artikel), $link, $data['id_komentar']);
+					$no++;
+				}
+			tutup_tabel();
 		break;
 
 		case "form":
