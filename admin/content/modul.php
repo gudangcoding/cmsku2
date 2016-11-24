81e3cbc982578c787f414b03afcca682ae556b6d
ResalR92 <resalramdahadi92@gmail.com>
2016-11-24 12:00:07 +0700

    Mengatur Aksi Tambah dan Edit Modul => Fitur => function_unzip.php => unzip_file()

--- admin/content/modul.php
+++ admin/content/modul.php
@@ -40,10 +40,54 @@
 
 		case "form":
 			//Skrip menampilkan form input dan edit data
+			if(isset($_GET['id'])){
+				$query = $mysqli->query("SELECT * FROM modul WHERE id_modul='$_GET[id]'");
+				$data = $query->fetch_array();
+				$aksi = "Edit";
+			}else{
+				$data = array("id_modul"=>"", "judul"=>"");
+				$aksi = "Tambah";
+			}
+
+			echo '<h3 class="page-header"><b>'.$aksi.' Modul</b></h3>';
+
+			buka_form($link, $data['id_modul'], strtolower($aksi));
+				buat_textbox("Judul", "judul", $data['judul']);
+				if($aksi == "Tambah") buat_textbox("File", "file", "", 4, "file");
+			tutup_form($link);
 		break;
 
 		case "action":
 			//Skrip menyisipkan atau mengedit data di database
+			if($_POST['aksi'] == "tambah"){
+				$filename = $_FILES["file"]["name"];
+				$source = $_FILES['file']['tmp_name'];
+				$type = $_FILES['file']['type'];
+
+				$nama = explode('.', $filename);
+				if($nama[1] != "zip"){
+					echo '<script>
+								window.alert("File yang diupload tidak bertipe .zip . Silahkan diulang.");
+								window.location.href=history.back();
+							</script>';
+				}else{
+					include "../library/function_unzip.php";
+					unzip_file($filename, $source, "../module/");
+
+					$mysqli->query("INSERT INTO modul SET 
+						judul = '$_POST[judul]',
+						folder = '$nama[0]',
+						aktif = 'N'
+						");
+
+					header('location:'.$link);
+				}
+			}elseif($_POST['aksi'] == "edit"){
+				$mysqli->query("UPDATE modul SET
+						judul 		= '$_POST[judul]'
+						WHERE id_modul='$_POST[id]'");			
+				header('location:'.$link);
+			}
 		break;
 
 		case "delete":
