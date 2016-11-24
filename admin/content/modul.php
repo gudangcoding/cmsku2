93cdf13deaf6427ec2bae6a5981c57bb300b9a25
ResalR92 <resalramdahadi92@gmail.com>
2016-11-24 10:35:25 +0700

    Menampilkan Data Modul

--- admin/content/modul.php
+++ admin/content/modul.php
@@ -10,6 +10,32 @@
 
 		default:
 			//Skrip menampilkan data
+			echo '<h3 class="page-header"><b>Daftar Modul</b>
+						<a href="'.$link.'&show=form" class="btn btn-primary btn-sm pull-right top-button">
+							<i class="glyphicon glyphicon-plus-sign"></i> Tambah
+						</a>
+					</h3>';
+
+			buka_tabel(array("Judul", "Aktif"));
+				$no = 1;
+				$query = $mysqli->query("SELECT * FROM modul ORDER BY aktif");
+
+				while($data = $query->fetch_array()){
+					if($data['aktif'] == 'Y')
+					{
+						$aktif = '<a href="'.$link.'&show=deactivate&id='.$data['id_modul'].'" style="color: green">
+										<i class="glyphicon glyphicon-ok-circle"></i>
+									</a>';
+					}else{
+						$aktif = '<a href="'.$link.'&show=activate&id='.$data['id_modul'].'" style="color: red">
+										<i class="glyphicon glyphicon-remove-circle"></i>
+									</a>';
+					}
+					isi_tabel($no, array($data['judul'], $aktif), $link, $data['id_modul']);
+
+					$no++;
+				}
+			tutup_tabel();
 		break;
 
 		case "form":
