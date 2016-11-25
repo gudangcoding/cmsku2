dc25f4bba8e898bf300aa682474e07e1da8b47e7
ResalR92 <resalramdahadi92@gmail.com>
2016-11-26 05:32:09 +0700

    Menampilkan Data User => Author

--- admin/content/user.php
+++ admin/content/user.php
@@ -7,6 +7,7 @@
 	switch($show){
 		default:
 			//Skrip menampilkan data
+			if($_SESSION['leveluser'] == "admin"){
 			echo '<h3 class="page-header"><b>Daftar User</b>
 						<a href="'.$link.'&show=form" class="btn btn-primary btn-sm pull-right top-button">
 							<i class="glyphicon glyphicon-plus-sign"></i> Tambah
@@ -25,10 +26,26 @@
 					$no++;
 				}
 			tutup_tabel();
+			}else{
+				$id_user = $_SESSION['id_user'];
+				$query = $mysqli->query("SELECT * FROM user WHERE id_user = '$_GET[id]'");
+				$data = $query->fetch_array();
+				$aksi = "Edit";
+
+				echo '<h3 class="page-header"><b>'.$aksi.' User</b></h3>';
+
+				buka_form($link,$id_user,strtolower($aksi));
+					buat_textbox("Nama Lengkap", "nama_lengkap",$data['nama_lengkap']);
+					buat_textbox("Email","email",$data['email']);
+					buat_textbox("Username","username",$data['username']);
+					buat_textbox("Password","password","",4, "password");
+				tutup_form($link);
+			}
 		break;
 
 		case "form":
 			//Skrip menampilkan form input dan edit data
+
 		break;
 
 		case "action":
