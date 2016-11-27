ba08db00e4d597ac3906951cfaaa858e8ec8e318
ResalR92 <resalramdahadi92@gmail.com>
2016-11-27 09:29:03 +0700

    Membuat Fungsi Mendapatkan Nama Folder yang Aktif

--- library/function_template.php
+++ library/function_template.php
@@ -8,3 +8,12 @@
 
 		return $setting['nilai'];
 	}
+	// Fungsi untuk mendapatkan nama folder template yang aktif
+	function folder_template()
+	{
+		global $mysqli;
+		$query = $mysqli->query("SELECT * FROM template WHERE aktif='Y'");
+		$tpl = $query->fetch_array();
+
+		return 'template/'.$tpl['folder'];
+	}
