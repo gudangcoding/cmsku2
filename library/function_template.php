9d1bb2e1e27465a8d991c61e63afef3f0846c265
ResalR92 <resalramdahadi92@gmail.com>
2016-11-27 09:44:16 +0700

    Membuat Fungsi Skrip Head meta

--- library/function_template.php
+++ library/function_template.php
@@ -17,3 +17,45 @@
 
 		return 'template/'.$tpl['folder'];
 	}
+	// Fungsi untuk memembuat meta header, judul website, memanggil bootstrap dan jquery
+	function meta_header()
+	{
+		global $mysqli;
+
+		$content = (isset($_GET['content'])) ? $_GET['content'] : "home";
+		if($content = "artikel"){
+			$qartikel = $mysqli->query("SELECT * FROM artikel WHERE id_artikel='$_GET[id]'");
+			$artikel = $qartikel->fetch_array();
+
+			$judul = $artikel['judul'].' - '.web_info('judul');
+			$deskripsi = $artikel['judul'];
+			$keyword = $artikel['tag'];
+		}else{
+			$judul = web_info('judul');
+			$deskripsi = web_info('deskripsi');
+			$keyword = web_info('keyword');
+		}
+
+		$icon = web_info('url')."/media/source/".web_info('ikon');
+		echo '<title>'.$judul.'</title>
+				
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+				<meta name="robots" content="index, follow">
+				<meta name="description" content="'.$deskripsi.'">
+				<meta name="keywords" content="'.$keyword.'">
+				<meta http-equiv="Copyright" content="'.web_info('url').'">
+				<meta name="author" content="Rohi">
+				<meta name="language" content="Indonesia">
+				<meta name="revisit-after" content="7">
+				<meta name="webcrawlers" content="all">
+				<meta name="rating" content="general">
+				<meta name="spiders" content="all">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				
+				<link rel="shortcut icon" href="'.$icon.'" />		
+				<link rel="stylesheet" type="text/css" href="'.web_info('url').'/plugin/bootstrap/css/bootstrap.min.css">
+				<link rel="stylesheet" type="text/css" href="'.web_info('url').'/'.folder_template().'/css/style.css">
+				
+				<script type="text/javascript" src="'.web_info('url').'/plugin/jquery/jquery-2.0.2.min.js"></script>
+				<script src="https://www.google.com/recaptcha/api.js"></script>';
+	}
